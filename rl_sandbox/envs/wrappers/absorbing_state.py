cc325e2174e99d83aa1057f8ff603b63c49c53b9
Bryan Chan <bryanchan13@gmail.com>
2020-12-10 10:31:47 -0500

    Better spot to reset done flag.
    Thanks Trevor :)

--- rl_sandbox/envs/wrappers/absorbing_state.py
+++ rl_sandbox/envs/wrappers/absorbing_state.py
@@ -20,14 +20,13 @@ class AbsorbingStateWrapper:
     def reset(self):
         self._curr_timestep = 0
         self._obs = self._env.reset()
+        self._done = False
         return self._get_obs()
 
     def step(self, action):
         self._curr_timestep += 1
         if self._create_absorbing_state and self._done:
-            obs = self._get_obs()
-            self._done = False
-            return obs, 0., True, {c.ABSORBING_STATE: True, c.DONE: False}
+            return self._get_obs(), 0., True, {c.ABSORBING_STATE: True, c.DONE: False}
 
         self._obs, reward, done, info = self._env.step(action)
         info[c.ABSORBING_STATE] = False
